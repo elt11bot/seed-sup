ff847b0a6559228ace616ebda16dbf6abb6dbbb5
elt11bot <aryaneskandari1381@gmail.com>
2016-03-09 20:35:43 +0330

    Added files via upload

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -1,150 +1,98 @@
+-- Saves the number of messages from a user
+-- Can check the number of messages with !stats 
+
 do
 
--- Returns a table with `name` and `msgs`
-local function get_msgs_user_chat(user_id, chat_id)
-  local user_info = {}
-  local uhash = 'user:'..user_id
-  local user = redis:hgetall(uhash)
-  local um_hash = 'msgs:'..user_id..':'..chat_id
-  user_info.msgs = tonumber(redis:get(um_hash) or 0)
-  user_info.name = user_print_name(user)..' ['..user_id..']'
-  return user_info
+local socket = require('socket') 
+local _file_stats = './data/stats.lua'
+local _stats
+
+function update_user_stats(msg)
+  -- Save user to stats table
+  local from_id = tostring(msg.from.id)
+  local to_id = tostring(msg.to.id)
+  local user_name = get_name(msg)
+  print ('New message from '..user_name..'['..from_id..']'..' to '..to_id)
+  -- If last name is nil dont save last_name.
+  local user_last_name = msg.from.last_name
+  local user_print_name = msg.from.print_name
+  if _stats[to_id] == nil then
+    print ('New stats key to_id: '..to_id)
+    _stats[to_id] = {}
+  end
+  if _stats[to_id][from_id] == nil then
+    print ('New stats key from_id: '..to_id)
+    _stats[to_id][from_id] = {
+      name = user_name,
+      last_name = user_last_name,
+      print_name = user_print_name,
+      msg_num = 1
+    }
+  else
+    print ('Updated '..to_id..' '..from_id)
+    local actual_num = _stats[to_id][from_id].msg_num
+    _stats[to_id][from_id].msg_num = actual_num + 1
+    -- And update last_name
+    _stats[to_id][from_id].last_name = user_last_name
+  end
 end
 
-local function chat_stats(chat_id)
-  -- Users on chat
-  local hash = 'chat:'..chat_id..':users'
-  local users = redis:smembers(hash)
-  local users_info = {}
-  -- Get user info
-  for i = 1, #users do
-    local user_id = users[i]
-    local user_info = get_msgs_user_chat(user_id, chat_id)
-    table.insert(users_info, user_info)
+function read_file_stats( )
+  local f = io.open(_file_stats, "r+")
+  -- If file doesn't exists
+  if f == nil then
+    -- Create a new empty table
+    print ('Created user stats file '.._file_stats)
+    serialize_to_file({}, _file_stats)
+  else
+    print ('Stats loaded: '.._file_stats)
+    f:close() 
   end
-  -- Sort users by msgs number
-  table.sort(users_info, function(a, b) 
-      if a.msgs and b.msgs then
-        return a.msgs > b.msgs
-      end
-    end)
-  local text = 'users in this chat \n'
-  for k,user in pairs(users_info) do
-    text = text..user.name..' = '..user.msgs..'\n'
-  end
-  local file = io.open("./groups/lists/"..chat_id.."stats.txt", "w")
-  file:write(text)
-  file:flush()
-  file:close() 
-  send_document("chat#id"..chat_id,"./groups/lists/"..chat_id.."stats.txt", ok_cb, false)
-  return --text
+  return loadfile (_file_stats)()
 end
 
-local function chat_stats2(chat_id)
-  -- Users on chat
-  local hash = 'chat:'..chat_id..':users'
-  local users = redis:smembers(hash)
-  local users_info = {}
 
-  -- Get user info
-  for i = 1, #users do
-    local user_id = users[i]
-    local user_info = get_msgs_user_chat(user_id, chat_id)
-    table.insert(users_info, user_info)
-  end
-
-  -- Sort users by msgs number
-  table.sort(users_info, function(a, b) 
-      if a.msgs and b.msgs then
-        return a.msgs > b.msgs
-      end
-    end)
-
-  local text = 'users in this chat \n'
-  for k,user in pairs(users_info) do
-    text = text..user.name..' = '..user.msgs..'\n'
-  end
-  return text
+local function save_stats()
+	-- Save stats to file
+	serialize_to_file(_stats, _file_stats)
 end
--- Save stats, ban user
-local function bot_stats()
 
-  local redis_scan = [[
-    local cursor = '0'
-    local count = 0
+local function get_stats_status( msg )
+	-- vardump(stats)
+	local text = ""
+  	local to_id = tostring(msg.to.id)
 
-    repeat
-      local r = redis.call("SCAN", cursor, "MATCH", KEYS[1])
-      cursor = r[1]
-      count = count + #r[2]
-    until cursor == '0'
-    return count]]
-
-  -- Users
-  local hash = 'msgs:*:'..our_id
-  local r = redis:eval(redis_scan, 1, hash)
-  local text = 'Users: '..r
-
-  hash = 'chat:*:users'
-  r = redis:eval(redis_scan, 1, hash)
-  text = text..'\nGroups: '..r
-  return text
+	for id, user in pairs(_stats[to_id]) do
+		if user.last_name == nil then
+			text = text..user.name.." ["..id.."]: "..user.msg_num.."\n"
+		else
+			text = text..user.name.." "..user.last_name.." ["..id.."]: "..user.msg_num.."\n"
+		end
+	end
+	print("usuarios: "..text)
+	return text
 end
+
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
-    local about = _config.about_text
-    local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
-    return about
-  end 
-  if matches[1]:lower() == "statslist" then
-    if not is_momod(msg) then
-      return "For mods only !"
-    end
-    local chat_id = msg.to.id
-    local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] requested group stats ")
-    return chat_stats2(chat_id)
-  end
-  if matches[1]:lower() == "stats" then
-    if not matches[2] then
-      if not is_momod(msg) then
-        return "For mods only !"
-      end
-      if msg.to.type == 'chat' then
-        local chat_id = msg.to.id
-        local name = user_print_name(msg.from)
-        savelog(msg.to.id, name.." ["..msg.from.id.."] requested group stats ")
-        return chat_stats(chat_id)
-      else
-        return
-      end
-    end
-    if matches[2] == "teleseed" then -- Put everything you like :)
-      if not is_admin(msg) then
-        return "For admins only !"
-      else
-        return bot_stats()
-      end
-    end
-    if matches[2] == "group" then
-      if not is_admin(msg) then
-        return "For admins only !"
-      else
-        return chat_stats(matches[3])
-      end
-    end
-  end
+	if matches[1] == "stats" then -- Hack
+    return get_stats_status(msg)
+	else 
+		print ("update stats")
+		update_user_stats(msg)
+    save_stats()
+	end
 end
+
+_stats = read_file_stats()
+
 return {
-  patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
-    }, 
-  run = run
+    description = "Numer of messages by user", 
+    usage = "!stats",
+    patterns = {
+      ".*",
+    	"^!(stats)"
+    	}, 
+    run = run 
 }
 
 end
